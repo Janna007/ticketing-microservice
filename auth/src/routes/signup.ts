2d26da447838a92434c08f535688b5885453f04c
janna <jannakondeth5@gmail.com>
2024-03-27 00:14:19 +0530

    bad req error handling

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,7 +1,9 @@
 import express,{Request,Response} from 'express'
 import { body,validationResult } from 'express-validator'
 import { RequestValidationError } from '../errors/request-validation-error'
-import { DatabaseConnectionError } from '../errors/database-connection-error'
+import { User } from '../models/user'
+import { BadRequestError } from '../errors/bad-request-error'
+
 
 
 const router=express.Router()
@@ -23,9 +25,21 @@ router.post('/api/users/signup',[
     }
     const {email,password}=req.body
 
-   console.log("creating user...")
-   throw new DatabaseConnectionError()
-    res.send({})  
+    const existingUser= await User.findOne({email})
+    if(existingUser){
+      throw new BadRequestError('user alreday exist with this email')
+    }
+
+   const user= User.build({
+       email,
+       password
+    })
+    await user.save()
+
+    res.status(201).send(user)
+
+
 })
 
 export  {router as signupRouter}
+
